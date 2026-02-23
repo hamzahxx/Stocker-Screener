095cebcc6277201f4f6a6b311f7be128a638f3b4
Hamzah <110326939+hamzahxx@users.noreply.github.com>
2026-02-23 17:43:41 +0530

    foundation - data_fetcher.py
    - takes an index string for input
    - calls nse's api to ask for stock list making that index
    - returns the stock list

---
+++ data_fetcher.py
@@ -0,0 +1,29 @@
+import requests
+
+def nse_stock_list_fetcher(index: str) -> list[str]:
+
+    index = index.upper()
+    index = index.replace(" ", "%20")
+    
+    session = requests.Session()
+
+    url = f"https://www.nseindia.com/api/equity-stockIndices?index={index}"
+    
+    headers = {
+        "User-Agent": "Mozilla/5.0",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Referer": "https://www.nseindia.com",
+    }
+    
+    response = session.get(url, headers=headers)
+    data = response.json()
+
+    # Filter only stocks (exclude the index entry which has no 'series' key)
+    stocks = [item for item in data["data"] if "series" in item]
+
+    stock_symbols = []
+
+    for stock in stocks:
+        stock_symbols.append(stock["symbol"])
+
+    return stock_symbols
