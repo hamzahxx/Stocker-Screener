dd79dad35ffe1b793b70c12043588b2273b3866b
Hamzah <110326939+hamzahxx@users.noreply.github.com>
2026-02-24 21:18:40 +0530

    fix

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from fastapi import FastAPI
 from routes.swing import router
 
 app = FastAPI(title="Stock Screener API")
-app.include_router(router, prefix="/")
+app.include_router(router, prefix="/screen")
