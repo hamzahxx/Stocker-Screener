2b99ef906a51818cc664ea6f32652d898c24b167
Hamzah <110326939+hamzahxx@users.noreply.github.com>
2026-03-13 22:08:41 +0530

    Metadata
    - Update Icon, Title, Description
    - Add OpenGraph's Icon, Title, Description
    - Add Twitter Metadata

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -18,8 +18,38 @@ const jetbrainsMono = JetBrains_Mono({
 });
 
 export const metadata: Metadata = {
-    title: "NSE Stock Screener",
-    description: "Dark-mode swing trade screener for NSE-listed equities",
+    title: "Stocker | NSE Screener",
+    description:
+        "Discover and analyze Indian stocks effortlessly with Stocker, your intuitive NSE screener. Instantly search by symbol, filter by index, and view real-time market status—all in a sleek, dark-mode interface designed for traders and investors.",
+    icons: {
+        icon: "/og-image.jpg",
+    },
+    themeColor: "#18181b",
+    openGraph: {
+        title: "Stocker | NSE Screener",
+        description:
+            "Discover and analyze Indian stocks effortlessly with Stocker, your intuitive NSE screener. Instantly search by symbol, filter by index, and view real-time market status—all in a sleek, dark-mode interface designed for traders and investors.",
+        url: "https://stocker.hamzahcodes.in/",
+        siteName: "Stocker",
+        images: [
+            {
+                url: "/og-image.jpg",
+                width: 1200,
+                height: 630,
+                alt: "Stocker NSE Screener",
+            },
+        ],
+        locale: "en_IN",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Stocker | NSE Screener",
+        description:
+            "Discover and analyze Indian stocks effortlessly with Stocker, your intuitive NSE screener. Instantly search by symbol, filter by index, and view real-time market status—all in a sleek, dark-mode interface designed for traders and investors.",
+        images: ["/og-image.jpg"],
+        site: "@copypastecoder7",
+    },
 };
 
 export default function RootLayout({
